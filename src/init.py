ba357cef6eab8912f3cb31a8cef35b41e3ece089
chunjoonsung <33312464+chunjoonsung@users.noreply.github.com>
2021-05-15 23:56:44 +0900

    Add files via upload

---
+++ src/init.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+class Sqlite():
+    def __init__(self,dbfile):
+        self.conn = sqlite3.connect(dbfile)
+    def execute(self,query):
+        self.conn.cursor().execute(query)
+    def select(self,query):
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+    def commit(self):
+        self.conn.commit()
+    def close(self):
+        self.conn.close()
+
+user_table = '''
+CREATE TABLE "user" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "username" VARCHAR(80) NOT NULL UNIQUE,
+    "password" VARCHAR(80) NULL,
+    "admin" INTEGER NULL
+);
+'''
+    
+if __name__ == "__main__":
+    sql = Sqlite("user.db")
+    sql.execute(user_table)
+    sql.execute("INSERT INTO user (username, password, admin) VALUES ('user','user1234','0')")
+    sql.execute("INSERT INTO user (username, password, admin) VALUES ('admin','nimda4321','1')")
+    sql.commit()
+    rows = sql.select(f"SELECT username, password FROM user")
+    for row in rows:
+        print(row)
+
+    
+    
+    
